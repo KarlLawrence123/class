157531f6218ada876c87329c885e54be7517ff9a
Law <law@lawdesktop.com>
2025-05-27 03:22:25 +0800

    fix the newscript

--- public/newscript.js
+++ public/newscript.js
@@ -1,5 +1,3 @@
-require('dotenv').config();
-
 // Add SheetJS library
 const script = document.createElement('script');
 script.src = 'https://cdn.sheetjs.com/xlsx-0.20.1/package/dist/xlsx.full.min.js';
