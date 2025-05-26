6f82cd8724a481560a92f5ec3bd784960f2ab074
Law <law@lawdesktop.com>
2025-05-27 03:08:59 +0800

    jwt_secret

--- routes/auth.js
+++ routes/auth.js
@@ -7,7 +7,7 @@ router.post('/login', async (req, res) => {
     if (email === adminEmail && password === adminPassword) {
         const token = jwt.sign(
             { role: 'admin' },
-            process.env.JWT_SECRET || 'your-secret-key',
+            process.env.JWT_SECRET || 'a6f5101e7c3804ec8fb4055f2eef26b2aaba14b8cce078f03fe5d25f43a763f8074abb237576f550636ca40fc4e0f51785a4ac85f36b20564f64069bf65b9ab8',
             { expiresIn: '1h' }
         );
 
